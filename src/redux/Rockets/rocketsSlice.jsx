96aafdb7256915c76e94e00a7b3e832d6ca9d11c
kenagy100@gmail.com <kenagy100@gmail.com>
2023-07-26 14:07:55 +0000

    Add RESERVE_ROCKET action to reducer

--- src/redux/Rockets/rocketsSlice.jsx
+++ src/redux/Rockets/rocketsSlice.jsx
@@ -2,6 +2,7 @@ import { createAsyncThunk, createReducer } from '@reduxjs/toolkit';
 import { URL } from '../../utils/constant';
 
 const FETCH_ROCKETS = 'rocketStore/rockets/FETCH_ROCKETS';
+const RESERVE_ROCKET = 'rocketStore/rockets/RESERVE_ROCKETS';
 
 export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async (post, { dispatch }) => {
   const response = await fetch(URL);
@@ -12,6 +13,7 @@ export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async (post, { dispa
     description: rocket.description,
     type: rocket.rocket_type,
     image: rocket.flickr_images[0],
+    reserved: false,
   }));
   dispatch({
     type: FETCH_ROCKETS,
@@ -20,15 +22,27 @@ export const fetchRockets = createAsyncThunk(FETCH_ROCKETS, async (post, { dispa
   return payload;
 })
 
+export const reserveRocket = (rocketId) => ({
+  type: RESERVE_ROCKET,
+  payload: rocketId,
+});
+
 const initialState = {
   rockets: [],
 }
 
 const rocketsSlice = createReducer(initialState, (builder) => {
-  builder.addCase(fetchRockets.fulfilled, (state, { payload }) => ({
+  builder
+    .addCase(fetchRockets.fulfilled, (state, { payload }) => ({
     ...state,
     rockets: [...payload],
-  }));
+    }))
+    .addCase(RESERVE_ROCKET, (state, { payload: rocketId }) => ({
+      ...state,
+      rockets: state.rockets.map((rocket) =>
+        rocket.id !== rocketId ? rocket : { ...rocket, reserved: true }
+      ),
+    }));
 })
 
 export default rocketsSlice;
